e6c5f3d2b4569c259a16192bce889ae21b8dec4c
FranciscaCMG <francisca.marquez@usach.cl>
2024-12-16 23:01:05 -0300

    [Fix] mongoDB

--- server.js
+++ server.js
@@ -1,40 +1,45 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const dotenv = require('dotenv')
-const cors = require('cors')
-const path = require('path')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+const path = require('path');
 
+dotenv.config();
+app.use(express.json());
 
-dotenv.config()
-app.use(express.json())
-
+// Configurar CORS según el entorno
 if (process.env.NODE_ENV === 'local') {
     app.use(cors({
         origin: 'http://localhost:5173',
         credentials: true
-    }))
+    }));
 } else {
     app.use(cors({
         credentials: true
-    }))
+    }));
 }
 
-app.use('/api', require('./routes/designRoutes'))
-app.use('/api', require('./routes/authRoutes'))
+// Configurar rutas
+app.use('/api', require('./routes/designRoutes'));
+app.use('/api', require('./routes/authRoutes'));
 
+// Configuración para producción
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "./frontend/dist")))
+    app.use(express.static(path.join(__dirname, "./frontend/dist")));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "./", "frontend", "dist", "index.html"))
-    })
+        res.sendFile(path.resolve(__dirname, "./", "frontend", "dist", "index.html"));
+    });
 }
 
+// Conectar a la base de datos
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(
-            'mongodb+srv://cwy:test123@cluster0.g8znc.mongodb.net/testdb?retryWrites=true&w=majority&appName=Cluster0'
-            );
+        const mongoURI = process.env.NODE_ENV === 'local'
+            ? process.env.LOCAL_DB_URI
+            : process.env.MONGODB_URI;
+
+        const conn = await mongoose.connect(mongoURI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(error);
@@ -42,8 +47,8 @@ const connectDB = async () => {
     }
 };
 
-connectDB()
+connectDB();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}..`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}..`));
